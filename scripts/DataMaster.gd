29c535bc4d55972edede990c4527b8b742c2f4ff
Tequila24 <ka4xid@gmail.com>
2026-08-25 17:02:42 +0400

    data loaded from jsons from wiki.gg

--- scripts/DataMaster.gd
+++ scripts/DataMaster.gd
@@ -4,152 +4,162 @@ extends Node
 const DEBUG = true
 
 
-@export var foxhole_database_path: String  = ""
-@export var _warden_faction_id: String
-@export var _colonial_faction_id: String
+const _VEHICLES_DATA_PATH: String  = "res://data/Foxhole Vehicles.json"
+const _ITEMS_DATA_PATH: String = "res://data/Foxhole Items.json"
+const _ARMAMENT_DATA_PATH: String = "res://data/Foxhole Armament.json"
 
-@export var _resistance_tier_id_enum_to_column_str: Dictionary[String, String] = {
-	"" = 				"", # None
-	"EArmourType::LightVehicle" = 			"light_vehicle_damage_mitigation",	# Unarmored
-	"EArmourType::Tier1Tank" = 				"tier1tank_damage_mitigation", # Lightly Armoured
-	"EArmourType::Tier2Tank" = 				"tier2tank_damage_mitigation", # Heavily Armoured
-	"EArmourType::Tier1Ship" = 				"tier1ship_damage_mitigation", # Ship
-	"EArmourType::Tier2Ship" = 				"tier2ship_damage_mitigation", # Armored Ship
-	"EArmourType::Tier1LargeShip" = 		"tier1large_ship_damage_mitigation", # Large Ships
-	"EArmourType::Tier1Aircraft" = 			"tier1aircraft_damage_mitigation", # Aircraft
-	"EArmourType::Trench" = 				"trench_damage_mitigation", # Trench
-	"EArmourType::Tier1Structure" = 		"tier1structure_damage_mitigation", # Structure Tier1
-	"EArmourType::Tier2Structure" = 		"tier2structure_damage_mitigation", # Structure Tier2
-	"EArmourType::Tier2BStructure" = 		"tier2bstructure_damage_mitigation", # Structure Tier2B
-	"EArmourType::Tier3Structure" = 		"tier3structure_damage_mitigation", # Structure Tier3
-	"EArmourType::Tier3BStructure" = 		"tier3bstructure_damage_mitigation", # Structure Tier3B
-	"EArmourType::HeavyBuildSite" = 		"heavy_build_site_damage_mitigation", # ???
-	"EArmourType::Tier1Garrison" = 			"tier1garrison_house_damage_mitigation", # GHouse T1
-	"EArmourType::Tier2Garrison" = 			"tier2garrison_house_damage_mitigation", # GHouse T2
-	"EArmourType::Tier3Garrison" = 			"tier3garrison_house_damage_mitigation", # GHouse T3
-	"EArmourType::WorldStructureHusk" =		"world_structure_husk_damage_mitigation" # ???
-}
 
-@export var _damage_type_name_to_id: Dictionary[String, String] = {
-	"Anti-Tank Explosive" = 	"AntiTankExplosive",
-	"Anti-Tank Kinetic" = 		"AntiTankKinetic",
-	"Armour Piercing" = 		"ArmourPiercing",
-	"Demolition" =				"Demolition",
-	"Explosive" = 				"Explosive",
-	"Fire" = 					"Fire",
-	"Heavy Kinetic" = 			"HeavyKinetic",
-	"High Explosive" = 			"HighExplosive",
-	"Incendiary" = 				"Incendiary",
-	"Light Kinetic" = 			"LightKinetic",
-	"Poisonous Gas" = 			"PoisonGas",
-	"Shrapnel" = 				"Shrapnel",
-	"Extinguishing" = 			"",
-	"Flare" = 					"",
-	"Smoke" = 					""
-}
 
-var foxhole_db: SQLite = SQLite.new()
+var _vehicles_data: Array[Variant]
+var _items_data: Array[Variant]
+var _armament_data: Array[Variant]
+
+
+var _all_vehicles: Dictionary[String, VehicleEntity] = {}
+var _all_items: Dictionary[String, ItemEntity] = {}
 
 
 
 func _ready() -> void:
+	_load_data()
 
-	foxhole_db.read_only = true
-	foxhole_db.path = foxhole_database_path
-	foxhole_db.open_db()
 
-	# var result = foxhole_db.select_rows("vehicles", "name != 'NULL' AND faction_variant == 'EFactionId::Wardens'", ["name"])
-	# for entry in result:
-	# 	print(entry)
+func _load_data():
+	var vehicles_file_str = FileAccess.get_file_as_string(_VEHICLES_DATA_PATH)
+	var armaments_file_str = FileAccess.get_file_as_string(_ARMAMENT_DATA_PATH)
+	var items_file_str = FileAccess.get_file_as_string(_ITEMS_DATA_PATH)
+	
+	_vehicles_data = JSON.parse_string(vehicles_file_str)
+	_armament_data = JSON.parse_string(armaments_file_str)
+	_items_data = JSON.parse_string(items_file_str)
 
-	# get_ammo_entity("LightTankAmmo")
+	_load_vehicles()
+	_load_items()
+	
+
+func _load_vehicles():
+	for vehicle_entry: Dictionary in _vehicles_data:
+		var new_vehicle: VehicleEntity = VehicleEntity.new()
+		if (vehicle_entry.get("codename") == null):
+			continue
+		new_vehicle.id = vehicle_entry.get("codename", "")
+		new_vehicle.name = vehicle_entry.get("name", "")
+		new_vehicle.image_name = vehicle_entry.get("image", "")
+		var faction_name = vehicle_entry.get("faction")
+		if (faction_name == "War"):
+			new_vehicle.faction = Enums.Faction.WARDEN
+		elif (faction_name == "Col"):
+			new_vehicle.faction = Enums.Faction.COLONIAL
+		else:
+			new_vehicle.faction = Enums.Faction.NEUTRAL
+
+
+		# Vitals
+		var vitals: ComponentVitals = ComponentVitals.new()
+		vitals.max_health = (vehicle_entry["vehicle hp"]) if (vehicle_entry["vehicle hp"] != null) else (0.0)
+		vitals.disable_threshold = (vehicle_entry["disable"] * 0.01) if (vehicle_entry["disable"] != null) else (0.0)
+		vitals.resistance_id = vehicle_entry["armour type"] if (vehicle_entry["armour type"] != null) else ("LightVehicle")
+		vitals.max_armor = (vehicle_entry["armour hp"]) if (vehicle_entry["armour hp"] != null) else (0.0)
+		new_vehicle.add_component(vitals)
+
+		# Armament
+		var vic_arm_entries: Array = _armament_data.filter(
+			func(arm_entry: Dictionary):
+				return arm_entry.get("parent_name", "") == new_vehicle.name
+		)
+		
+		for arm_entry: Dictionary in vic_arm_entries:
+			var used_ammo_ids: Array[String] = []
+			for i in range(5):
+				var ammo_id: String = arm_entry.get("AmmoName%d" % i, "")
+				if (not ammo_id.is_empty()):
+					used_ammo_ids.append(ammo_id)
+
+			if (used_ammo_ids.is_empty()):
+				print("No ammo used, skipping")
+				continue
+
+			var new_gun: ComponentGun = ComponentGun.new()
+			new_gun.ammo_used_ids.assign(used_ammo_ids)
+			new_gun.magazine_size = arm_entry.get("MagazineSize", 1)
+			new_gun.cooldown_duration_s = arm_entry.get("FiringTime", 0)
+			new_gun.reload_duration_s = arm_entry.get("ReloadTime", 0)
+			new_gun.damage_modifier = 1.0 + float(arm_entry.get("VelocityMod", 0.0))
+
+			new_vehicle.add_component(new_gun)
+
+		
+		_all_vehicles[new_vehicle.id] = new_vehicle
+
+
+func _load_items():
+	for item_entry: Dictionary in _items_data:
+		if (item_entry["codename"] == null):
+			continue
+
+		var new_item: ItemEntity = ItemEntity.new()
+		new_item.id = item_entry["codename"]
+		new_item.name = item_entry["name"]
+		new_item.image_name = item_entry["image"]
+		var faction_name = item_entry["faction"]
+		if (faction_name == "War"):
+			new_item.faction = Enums.Faction.WARDEN
+		elif (faction_name == "Col"):
+			new_item.faction = Enums.Faction.COLONIAL
+		else:
+			new_item.faction = Enums.Faction.NEUTRAL
+
+		if (item_entry["damage"] != null):
+			var new_damage_component: ComponentDamage = ComponentDamage.new()
+			new_damage_component.raw_damage = item_entry["damage"]
+			new_damage_component.damage_type_id = item_entry["damage type"]
+			new_item.add_component(new_damage_component)
+
+		_all_items[new_item.name] = new_item
+
 
 
 func get_resistance_to_damage_type(tier_id: String, damage_type_id: String) -> float:
-	var tier_column_id: String = _resistance_tier_id_enum_to_column_str.get(tier_id, "light_vehicle_damage_mitigation")
-	var resistances_for_tier_result: Array[Dictionary] = foxhole_db.select_rows("damageprofiles", "id == '%s'" % damage_type_id, ["%s" % tier_column_id])
+	return 0
+	# var tier_column_id: String = _resistance_tier_id_enum_to_column_str.get(tier_id, "light_vehicle_damage_mitigation")
+	# var resistances_for_tier_result: Array[Dictionary] = foxhole_db.select_rows("damageprofiles", "id == '%s'" % damage_type_id, ["%s" % tier_column_id])
 
-	return (resistances_for_tier_result.get(0)[tier_column_id] as float)
+	# return (resistances_for_tier_result.get(0)[tier_column_id] as float)
 
 
 
 func get_all_vehicle_entities() -> Array[VehicleEntity]:
-	var result: Array[VehicleEntity] = []
-
-	var all_vics = foxhole_db.select_rows("vehicles", "name != 'NULL'", ["id", "name", "armour_type", "max_health", "minor_damage_percent", "iconobject_path"])
-	
-	for vehicle in all_vics:
-		var new_entity: VehicleEntity = VehicleEntity.new()
-
-		# print(vehicle.name)
-
-		new_entity.id = vehicle.id
-		new_entity.name = vehicle.name
-		new_entity.icon_path = (vehicle.iconobject_path as String).substr(24).left(-1) + "tga"
-
-		var new_vitals_component: ComponentVitals = ComponentVitals.new()
-		new_vitals_component.max_health = vehicle.max_health
-		new_vitals_component.max_armor = 0
-		new_vitals_component.disable_threshold = vehicle.minor_damage_percent if vehicle.minor_damage_percent < 1 else 1
-		new_vitals_component.resistance_id = vehicle.armour_type
-		new_entity.add_component(new_vitals_component)
-
-		var all_vehicle_additional_seats = foxhole_db.select_rows("mounts_seats", "parent == '%s'" % vehicle.id, ["mount"])
-		for seat in all_vehicle_additional_seats:
-			var all_gun_mounts = foxhole_db.select_rows("mounts", "id == '%s'" % seat.mount, ["ammo_name", "multi_ammo", "max_ammo", "damage_multiplier", "firing_durationdelaybetweenrefire", "reload_duration"])
-
-			for gun_mount in all_gun_mounts:
-				var new_attack_component: ComponentGun = ComponentGun.new()
-				var all_ammo_possible: Array[String] = []
-				if (gun_mount.ammo_name):
-					all_ammo_possible.append(gun_mount.ammo_name)
-				if (gun_mount.multi_ammo != null && not gun_mount.multi_ammo.is_empty()):
-					gun_mount.multi_ammo = gun_mount.multi_ammo.substr(1, gun_mount.multi_ammo.length()-2)
-					for ammo_str in gun_mount.multi_ammo.rsplit(", ", true, 0):
-						ammo_str = ammo_str.substr(1, ammo_str.length()-2)
-						all_ammo_possible.append(ammo_str)
-
-				if all_ammo_possible.is_empty():
-					continue
-
-				# print("\t%s" % str(all_ammo_possible))
-
-				new_attack_component.ammo_used_ids = all_ammo_possible
-				new_attack_component.magazine_size = gun_mount.max_ammo
-				new_attack_component.reload_duration_s = gun_mount.reload_duration
-				new_attack_component.cooldown_duration_s = 0.25 if gun_mount.firing_durationdelaybetweenrefire == 0 else gun_mount.firing_durationdelaybetweenrefire
-				new_attack_component.damage_modifier = gun_mount.damage_multiplier
-				new_entity.add_component(new_attack_component)
-		
-		result.append(new_entity)
-
-	return result
+	return _all_vehicles.values()
 
 
-func get_ammo_entity(id: String) -> ItemEntity:
-	var ammo_entity: ItemEntity = ItemEntity.new()
+func get_vehicle_entity(id: String) -> VehicleEntity:
+	return _all_vehicles.get(id, VehicleEntity.new())
 
-	var quiery_result = foxhole_db.select_rows("ammo", "id == '%s'" % id, ["name", "damage", "damage_type_display_name", "iconobject_path"])
-	if (quiery_result.is_empty()):
-		if (DEBUG): print("Error processing %s id!" % id)
-		return null
 
-	ammo_entity.id = id
-	ammo_entity.name = quiery_result.get(0).name
-	ammo_entity.icon_path = quiery_result.get(0).iconobject_path
+func get_item_entity(id: String) -> ItemEntity:
+	var item: ItemEntity = ItemEntity.new()
 
-	var new_damage_component: ComponentDamage = ComponentDamage.new()
-	new_damage_component.raw_damage = quiery_result.get(0).damage
-	new_damage_component.damage_type_id = _damage_type_name_to_id.get(quiery_result.get(0).damage_type_display_name, "Light Kinetic")
-	ammo_entity.add_component(new_damage_component)
+	# var quiery_result = foxhole_db.select_rows("ammo", "id == '%s'" % id, ["name", "damage", "damage_type_display_name", "iconobject_path"])
+	# if (quiery_result.is_empty()):
+	# 	if (DEBUG): print("Error processing %s id!" % id)
+	# 	return null
 
-	# print(quiery_result.get(0))
-	# print(quiery_result.get(0).name)
-	# print(quiery_result.get(0).damage)
-	# print(type_string(typeof(quiery_result.get(0))))
+	# ammo_entity.id = id
+	# ammo_entity.name = quiery_result.get(0).name
+	# ammo_entity.icon_path = quiery_result.get(0).iconobject_path
 
-	return ammo_entity
+	# var new_damage_component: ComponentDamage = ComponentDamage.new()
+	# new_damage_component.raw_damage = quiery_result.get(0).damage
+	# new_damage_component.damage_type_id = _damage_type_name_to_id.get(quiery_result.get(0).damage_type_display_name, "Light Kinetic")
+	# ammo_entity.add_component(new_damage_component)
+
+	# # print(quiery_result.get(0))
+	# # print(quiery_result.get(0).name)
+	# # print(quiery_result.get(0).damage)
+	# # print(type_string(typeof(quiery_result.get(0))))
+
+	return item
 
 
 func _exit_tree() -> void:
-	foxhole_db.close_db()
+	pass
